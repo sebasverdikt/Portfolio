bf475dc9658ebd3c34095df501841aa4bfddedab
sebasverdikt <sverdikt@gmail.com>
2022-12-20 20:49:23 +0100

    update imgs & index

--- lightbox.js
+++ lightbox.js
@@ -41,18 +41,18 @@
   // Descriptions of all options available on the demo site:
   // http://lokeshdhakar.com/projects/lightbox2/index.html#options
   Lightbox.defaults = {
-    albumLabel: 'Image %1 of %2',
+    albumLabel: '%1 de %2',
     alwaysShowNavOnTouchDevices: false,
-    fadeDuration: 600,
+    fadeDuration: 300,
     fitImagesInViewport: true,
-    imageFadeDuration: 600,
+    imageFadeDuration: 300,
     // maxWidth: 800,
     // maxHeight: 600,
     positionFromTop: 50,
     resizeDuration: 700,
     showImageNumberLabel: true,
-    wrapAround: false,
-    disableScrolling: false,
+    wrapAround: true,
+    disableScrolling: true,
     /*
     Sanitize Title
     If the caption data is trusted, for example you are hardcoding it in, then leave this to false.
