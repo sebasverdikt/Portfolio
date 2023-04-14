f49400abfd2b99fb5298471ddd7584d1ec81fee2
sebasverdikt <sverdikt@gmail.com>
2023-04-14 17:44:26 +0200

    Update orders-analyzer.js

--- lab/orders-analyzer.js
+++ lab/orders-analyzer.js
@@ -6,37 +6,37 @@ const total = {};
 const avg = {};
 
 weekdays.forEach(weekday => {
-    count[weekday] = 0;
-    total[weekday] = 0;
-    avg[weekday] = 0;
+  count[weekday] = 0;
+  total[weekday] = 0;
+  avg[weekday] = 0;
 });
 
 orders.orders.forEach(order => {
-    const creationDate = new Date(order.creationDate);
-    const weekday = weekdays[creationDate.getDay()];
-    order.orderLines.forEach(orderLine => {
-        count[weekday] += orderLine.quantity;
-        total[weekday] += orderLine.unitPrice * orderLine.quantity;
-    });
+  const creationDate = new Date(order.creationDate);
+  const weekday = weekdays[creationDate.getDay()];
+  order.orderLines.forEach(orderLine => {
+    count[weekday] += orderLine.quantity;
+    total[weekday] += orderLine.unitPrice * orderLine.quantity;
+  });
 });
 
 weekdays.forEach(weekday => {
-    total[weekday] = (Math.round(total[weekday] * 100) / 100).toFixed(2);
-    if (total[weekday] == 0) {
-        total[weekday] = "0";
-    }
-    avg[weekday] = count[weekday] > 0 ? (Math.round((total[weekday] / count[weekday]) * 100) / 100).toFixed(2) : "0";
+  total[weekday] = (Math.round(total[weekday] * 100) / 100).toFixed(2);
+  if (total[weekday] == 0) {
+    total[weekday] = "0";
+  }
+  avg[weekday] = count[weekday] > 0 ? (Math.round((total[weekday] / count[weekday]) * 100) / 100).toFixed(2) : "0";
 });
 
 const list = document.createElement('ul');
 weekdays.forEach(weekday => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-    ${weekday}:<br>
-      count: ${count[weekday]}<br>
-      total: ${total[weekday]}<br>
-      avg: ${avg[weekday]}`;
-    list.appendChild(listItem);
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `
+  ${weekday}:<br>
+  count: ${count[weekday]}<br>
+  total: ${total[weekday]}<br>
+  avg: ${avg[weekday]}`;
+  list.appendChild(listItem);
 });
 
 document.body.appendChild(list);
